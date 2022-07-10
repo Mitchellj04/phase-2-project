d5b4dae55f26ea0cb3621c47f57e3c474e70c1f7
JustinMitchell <mitchelljm04@gmail.com>
2022-07-09 21:21:38 -0400

    About Me added

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import './About.css'
 
 const About = () => {
   return (
-    <div>
-        <div>
-            <span></span>
+    <div className='aboutMe'>
+        <div className='aboutMeItem'>
+            <span className='aboutMeTitle'>ABOUT ME</span>
+            <img /> 
+            <p>
+            I have been passionate about the outdoors since I was a kid. My family would take an annual camping trip to the Adirondecks.
+            This inspired me to try and travel more, since then I have been to majority of the carribean, all of the east coast, and even Australia. 
+            I plan to continue to travel the world and see new and exciting places. 
+            </p>
+        </div>
+        <div className='aboutMeItem'>
+            <span className='aboutMeTitle'>CATEGORY</span>
+            <ul className='aboutMeList'>
+                <li className='listitem'>Mountains</li>
+                <li className='listitem'>Camping</li>
+                <li className='listitem'>Beach</li>
+                <li className='listitem'>City</li>
+            </ul>
         </div>
     </div>
   )
--- src/component/Pages/Home.js
+++ src/component/Pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../Header/Header'
+import About from '../About/About'
 
 const Home = () => {
   return (
     <>
     <Header />
+    <div>
+        <About />
+    </div>
     </>
   )
 }
