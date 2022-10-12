53de90a32c5810c210fc128bb648023da9ce3f9d
JustinMitchell <mitchelljm04@gmail.com>
2022-10-12 14:30:07 -0400

    Test Deleted

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import Header from '../Header/Header'
 import Content from '../Posts/Content/Content'
 import About from '../About/AboutMe'
-import Test from "../Test"
 
 function Home({postList, setTitle, title}) {
 
